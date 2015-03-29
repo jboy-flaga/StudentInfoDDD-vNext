7ee97c914a3181fa79147725f11905a5b7726af0
jboyflaga <jeremiahflaga@gmail.com>
2015-03-29 21:19:51 +0800

    Modified ValueObject class for CoreCLR

--- src/StudentInfo.SharedKernel/ValueObject.cs
+++ src/StudentInfo.SharedKernel/ValueObject.cs
@@ -7,93 +7,105 @@ using System.Reflection;
 
 namespace StudentInfo.SharedKernel
 {
-    public abstract class ValueObject<T> //: IEquatable<T>
-      where T : ValueObject<T>
-    {
-        //public override bool Equals(object obj)
-        //{
-        //    if (obj == null)
-        //        return false;
+	public abstract class ValueObject<T> //: IEquatable<T>
+	  where T : ValueObject<T>
+	{
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
 
-        //    T other = obj as T;
+			T other = obj as T;
 
-        //    return Equals(other);
-        //}
+			return Equals(other);
+		}
 
-        //public override int GetHashCode()
-        //{
-        //    IEnumerable<FieldInfo> fields = GetFields();
+		public override int GetHashCode()
+		{
+			IEnumerable<FieldInfo> fields = GetFields();
 
-        //    int startValue = 17;
-        //    int multiplier = 59;
+			int startValue = 17;
+			int multiplier = 59;
 
-        //    int hashCode = startValue;
+			int hashCode = startValue;
 
-        //    foreach (FieldInfo field in fields)
-        //    {
-        //        object value = field.GetValue(this);
+			foreach (FieldInfo field in fields)
+			{
+				object value = field.GetValue(this);
 
-        //        if (value != null)
-        //            hashCode = hashCode * multiplier + value.GetHashCode();
-        //    }
+				if (value != null)
+					hashCode = hashCode * multiplier + value.GetHashCode();
+			}
 
-        //    return hashCode;
-        //}
+			return hashCode;
+		}
 
-        //public virtual bool Equals(T other)
-        //{
-        //    if (other == null)
-        //        return false;
+		public virtual bool Equals(T other)
+		{
+			if (other == null)
+				return false;
 
-        //    Type t = GetType();
-        //    Type otherType = other.GetType();
+			Type t = GetType();
+			Type otherType = other.GetType();
 
-        //    if (t != otherType)
-        //        return false;
+			if (t != otherType)
+				return false;
 
-        //    FieldInfo[] fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+#if ASPNET50
+			FieldInfo[] fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+#elif ASPNETCORE50
+			TypeInfo info = t.GetTypeInfo();
+			IEnumerable<FieldInfo> fields = info.DeclaredFields;
+#endif
 
-        //    foreach (FieldInfo field in fields)
-        //    {
-        //        object value1 = field?.GetValue(other);
-        //        object value2 = field.GetValue(this);
+			foreach (FieldInfo field in fields)
+			{
+				object value1 = field?.GetValue(other);
+				object value2 = field.GetValue(this);
 
-        //        if (value1 == null)
-        //        {
-        //            if (value2 != null)
-        //                return false;
-        //        }
-        //        else if (!value1.Equals(value2))
-        //            return false;
-        //    }
+				if (value1 == null)
+				{
+					if (value2 != null)
+						return false;
+				}
+				else if (!value1.Equals(value2))
+					return false;
+			}
 
-        //    return true;
-        //}
+			return true;
+		}
 
-        //private IEnumerable<FieldInfo> GetFields()
-        //{
-        //    Type t = GetType();
+		private IEnumerable<FieldInfo> GetFields()
+		{
+			Type t = GetType();
 
-        //    List<FieldInfo> fields = new List<FieldInfo>();
+			List<FieldInfo> fields = new List<FieldInfo>();
 
-        //    while (t != typeof(object))
-        //    {
-        //        fields.AddRange(t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public));
+			while (t != typeof(object))
+			{
+#if ASPNET50
+				fields.AddRange(t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public));
 
-        //        t = t.BaseType;
-        //    }
+				t = t.BaseType;
+#elif ASPNETCORE50
+				TypeInfo typeInfo = t.GetTypeInfo();
+				fields.AddRange(typeInfo.DeclaredFields);
 
-        //    return fields;
-        //}
+				t = typeInfo.BaseType;
+#endif
+			}
 
-        //public static bool operator ==(ValueObject<T> x, ValueObject<T> y)
-        //{
-        //    return x.Equals(y);
-        //}
+			return fields;
+		}
 
-        //public static bool operator !=(ValueObject<T> x, ValueObject<T> y)
-        //{
-        //    return !(x == y);
-        //}
-    }
+		public static bool operator ==(ValueObject<T> x, ValueObject<T> y)
+		{
+			return x.Equals(y);
+		}
+
+		public static bool operator !=(ValueObject<T> x, ValueObject<T> y)
+		{
+			return !(x == y);
+		}
+	}
 }
